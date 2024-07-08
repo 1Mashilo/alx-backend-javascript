d6ca3bc752173666f3cfb38c955be3d1ecf492b8
1mashilo <christophermashilo@gmail.com>
2024-07-08 08:26:29 +0000

    task 3

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 const getStudentsByLocation = (students, city) => {
-  const arratyObjec = students.filter((funtion) => funtion.location === city);
-  return arratyObjec;
+  const studentsByCity = students.filter(student => student.location === city);
+  return studentsByCity;
 };
 
 export default getStudentsByLocation;
