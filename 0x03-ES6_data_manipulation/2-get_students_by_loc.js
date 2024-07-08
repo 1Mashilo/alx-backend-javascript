a9c17a87d7165d5ed16f585ba63ba374450440de
1mashilo <christophermashilo@gmail.com>
2024-07-08 09:04:00 +0000

    lint errors

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,6 @@
 const getStudentsByLocation = (students, city) => {
-  const studentsByCity = students.filter(student => student.location === city);
+  const studentsByCity = students.filter((student) => student.location === city);
   return studentsByCity;
 };
 
 export default getStudentsByLocation;
-
