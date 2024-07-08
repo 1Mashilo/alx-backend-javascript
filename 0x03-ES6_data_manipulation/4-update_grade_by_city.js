8ebb048b8dc210a90bf5dab669b289eb99793cc4
1Mashilo <christophermashilo@gmail.com>
2024-07-08 12:12:57 +0200

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    return students
+        .filter(student => student.city === city)
+        .map(student => {
+            const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+            const grade = gradeObj ? gradeObj.grade : 'N/A';
+            return { ...student, grade };
+        });
+}
