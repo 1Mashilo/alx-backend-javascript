0cd721f4cb197fa5a7294a8f6e75324f4ba49e03
1mashilo <christophermashilo@gmail.com>
2024-08-20 19:21:01 +0000

    ✅ Complete task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-    const name = input.toString().trim();
-    console.log(`Your name is: ${name}`);
-    process.stdout.write("This important software is now closing\n");
-    process.exit();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
